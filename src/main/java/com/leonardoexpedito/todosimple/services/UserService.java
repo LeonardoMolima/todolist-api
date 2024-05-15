bc91f1b877dc608d5a61c7b3d102018b9d07e391
Leonardo Lima <Leonardomolima1@gmail.com>
2024-05-14 23:41:29 -0300

    Refactor User Service

--- src/main/java/com/leonardoexpedito/todosimple/services/UserService.java
+++ src/main/java/com/leonardoexpedito/todosimple/services/UserService.java
@@ -1,6 +1,5 @@
 package com.leonardoexpedito.todosimple.services;
 
-import com.leonardoexpedito.todosimple.repositories.TaskRepository;
 import com.leonardoexpedito.todosimple.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,9 +15,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException(
@@ -30,7 +26,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll((obj.getTasks()));
         return obj;
     }
 
