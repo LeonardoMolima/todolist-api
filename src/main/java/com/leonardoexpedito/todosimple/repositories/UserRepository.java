5d8351f8abb4154479372df06c71e942e2d22907
Leonardo Lima <Leonardomolima1@gmail.com>
2024-05-14 23:01:29 -0300

    Create User and Task repositories

---
+++ src/main/java/com/leonardoexpedito/todosimple/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.leonardoexpedito.todosimple.repositories;
+
+import com.leonardoexpedito.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
